a6e9034aab7ad3db64e4fba02d72b064560adcbf
algalyq <algalyq@mail.ru>
2024-03-16 15:54:57 +0600

    upd: lifetime token

--- main/local_settings.py
+++ main/local_settings.py
@@ -43,7 +43,7 @@ REST_FRAMEWORK = {
     ],
 }
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
