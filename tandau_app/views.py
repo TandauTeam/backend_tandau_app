904d21d1f875a531716ba0a722a26a753f4dd720
algalyq <algalyq@mail.ru>
2024-03-21 23:04:45 +0600

    fix:url p2

--- tandau_app/views.py
+++ tandau_app/views.py
@@ -318,7 +318,7 @@ class UserLoginAPIView(APIView):
                     "email":email,
                     "password":password
                 }
-                url = "http://127.0.0.1:8001/tandau/login/v1"
+                url = "https://tandauapp-production.up.railway.app/tandau/login/v1"
                 print(data)
                 response = requests.post(url, data)
                 if response.status_code == 200:
